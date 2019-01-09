bfe67e4f1b9c5e9f09e0f29f196ac9fc67060e12
rhaegar453 <shivarajapple@gmail.com>
2019-01-09 22:54:53 +0530

    Application Running successfully and deployed on Heroku

---
+++ build/precache-manifest.f686dc9d1ad9ab722b374b2a7f917ac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26af74df024fc4e78018",
+    "url": "/static/js/main.26af74df.chunk.js"
+  },
+  {
+    "revision": "64fed08bc4ccebbb0088",
+    "url": "/static/js/1.64fed08b.chunk.js"
+  },
+  {
+    "revision": "26af74df024fc4e78018",
+    "url": "/static/css/main.c6ce5fca.chunk.css"
+  },
+  {
+    "revision": "dd7c7ac1339b3697abfde9087fc91016",
+    "url": "/index.html"
+  }
+];
